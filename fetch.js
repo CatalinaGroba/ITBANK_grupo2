00545ae815125c1bb738ae56be69775ad4c4e36e
suray pedraza <suray_pedraza@hotmail.com>
2022-06-16 00:00:07 -0300

    Added Currencies Filter

--- fetch.js
+++ fetch.js
@@ -1,10 +1,18 @@
 const fetchCurrencies = () => {
-  fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
-  .then((currencies) => currencies.json())
-  .then((currencies) => {
-    console.log(currencies);
-    /* aca iria el la funcion que filtra y la funcion que genera el html */
-  })
+  fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
+    .then((currencies) => currencies.json())
+    .then((currencies) => {
+  
+      /* aca iria el la funcion que genera el html */
+    const filteredCurrencies = filterMainCurrencies(currencies)
+    });
 };
-
-fetchCurrencies();
+const filterMainCurrencies = (currencies) => {
+  return currencies.filter(
+    (currencie) =>
+      currencie.casa.nombre == "Dolar Oficial" ||
+      currencie.casa.nombre == "Dolar Blue" ||
+      currencie.casa.nombre == "Dolar turista"
+  );
+};
+fetchCurrencies(); 
