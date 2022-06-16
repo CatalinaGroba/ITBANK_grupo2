8ca77204bc27caed298128875782489d91f1b941
Ignacio Latorre <ignaciolatorreap@gmail.com>
2022-06-15 22:42:53 -0300

    fetch changes + LEER comentarios

--- fetch.js
+++ fetch.js
@@ -1,10 +1,14 @@
-const fetchCurrencies = () => {
-  fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
+const currenciesAPI = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales';
+
+const fetchCurrencies = (url, callback) => {
+  fetch(url)
   .then((currencies) => currencies.json())
   .then((currencies) => {
     console.log(currencies);
-    /* aca iria el la funcion que filtra y la funcion que genera el html */
+    callback(currencies)
+    /* callback seria la funcion que filtra + la funcion que genera el html */
+    /* hay que hacer el filtro y el html en otro archivo, asi separamos la logica para que no sea todo un quilombo */
   })
 };
 
-fetchCurrencies();
+fetchCurrencies(currenciesAPI, () => {});
